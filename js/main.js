4412124538f51b2dfcc2453f0f92a5b0f059b318
ptmartins <pedro@pedromartins.com.pt>
2020-10-29 17:21:42 +0000

    Delete unused files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let options = {
-        bgColor: '#53f6c7',
-        color: '#1d1b24',
-        fontSize: 2.8,
-		size: 50
-    };
-    scrollToTop.init(options);   
+(function() {
 
-    let menuBtn = document.getElementById('menuBtn'),
+    let options = {
+            bgColor: '#53f6c7',
+            color: '#1d1b24',
+            fontSize: 2.8,
+            size: 50
+        },
+        menuBtn = document.getElementById('menuBtn'),
         sideNav = document.getElementById('sideNav'),
         closeBtn = document.getElementById('closeBtn'),
-        links = document.getElementsByClassName('sideNav-link');
+        links = document.getElementsByClassName('sideNav-link'),
 
+        showMenu = function() {
+            sideNav.classList.toggle('open');
+        },
 
-    let showMenu = function() {
-        sideNav.classList.toggle('open');
-    }
+        addEventListeners = function() {
+            menuBtn.addEventListener('click', showMenu);
+            closeBtn.addEventListener('click', showMenu);
+            for(let i = 0; i < links.length; i++) {
+                links[i].addEventListener('click', showMenu);
+            }
+        },
+    
+        init = function() {
+            scrollToTop.init(options); 
+            addEventListeners();
+        };
 
-    menuBtn.addEventListener('click', showMenu);
-    closeBtn.addEventListener('click', showMenu);
-    for(let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', showMenu);
-    }
-});
+    window.addEventListener('DOMContentLoaded', init);
+})()
